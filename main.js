8c4566a2c9dc071dd675bc1a4b44c26cde04641b
Lin <judy86422@gmail.com>
2024-06-07 17:26:47 +0800

    移除js測試資訊

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 //表單 輸入錯誤監聽
 let infoForm = document.getElementById('info_form');
-console.log(infoForm.nodeName);
-let content = document.querySelectorAll('.info_form_items');
-console.log(content[0].value);
-let icons = document.querySelectorAll('.error-icon');
-console.log(icons[1].className);
-let warm = document.querySelectorAll('.error-warm');
-console.log(warm[2].textContent);
 
 infoForm.addEventListener('submit',(e) => {
     let content = document.querySelectorAll('.info_form_items');
